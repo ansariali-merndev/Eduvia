290cb783e3b115626fa949fdd70ba2969445e2eb
ansariali <=>
2026-02-15 10:37:29 +0530

    first commit

---
+++ server/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    content = {
+        "success": True,
+        "message": "Welcome back to the FASTAPI application"
+    }
+    return JSONResponse(
+        status_code=200,
+        content=content
+    )
